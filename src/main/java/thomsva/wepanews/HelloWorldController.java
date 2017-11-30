44a84f482b020fe8942ddb242413a8d0e88c1b63
Thomas Svartbäck <thomas.svartback@helsinki.fi>
2017-11-30 16:41:01 +0200

    helloworld

---
+++ src/main/java/thomsva/wepanews/HelloWorldController.java
@@ -0,0 +1,20 @@
+
+package thomsva.wepanews;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+    
+@GetMapping("*")
+    @ResponseBody
+    public String home() {
+        return "Hello World!";
+    }
+}
+
+
+
---
+++ src/main/java/thomsva/wepanews/WepaNewsApplication.java
@@ -0,0 +1,13 @@
+
+package thomsva.wepanews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WepaNewsApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WepaNewsApplication.class, args);
+    }
+    
+}
